e0ebcddb21832e484e39709b24f0819db4272b32
sarika <patilsarikar96@gmail.com>
2022-06-29 11:30:23 +0530

    change

--- src/test/java/Test/Login_Testcases.java
+++ src/test/java/Test/Login_Testcases.java
@@ -83,4 +83,12 @@ public class Login_Testcases extends listnersBaseTest{
         String status= zerodha_loginPage.clickonlogin();
         System.out.println(status);
 	}
+
+	public void loginwTestdata() {
+		Zerodha_LoginPage zerodha_loginPage=new Zerodha_LoginPage(driver) ;
+		zerodha_loginPage.clickonSubmit();
+        String status= zerodha_loginPage.clickonlogin();
+        System.out.println(status);
+}
+
 }
